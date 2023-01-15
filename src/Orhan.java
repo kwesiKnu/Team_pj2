d0127ae19ad126656ef68a687796de98d8b459d9
orhankirmiziny <kirmizi.orhan@gmail.com>
2023-01-15 10:52:05 -0500

    created a new class

---
+++ src/Orhan.java
@@ -0,0 +1,5 @@
+public class Orhan {
+
+    // created first class
+
+}
--- src/Pj_test.java
+++ src/Pj_test.java
@@ -3,4 +3,5 @@ public class Pj_test {
     // added a comment
     // added a second comment
     // adding a comment -orh-
+
 }
